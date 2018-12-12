2e961edeaa1517a45f35d1519673413a14575dda
lifujiang <q867007988@gmail.com>
2018-12-12 21:25:55 +0800

    优化结构, 增加了注释, 增强语义化

--- nodejs/crud-express/app.js
+++ nodejs/crud-express/app.js
@@ -2,24 +2,32 @@
 * @Author: lifujiang
 * @Date:   2018-12-11 11:09:01
 * @Last Modified by:   lifujiang
-* @Last Modified time: 2018-12-11 14:53:27
+* @Last Modified time: 2018-12-12 19:57:51
 */
 
+// 加载模块
 var express = require('express')
-var app = express()
 var router = require('./router')
 var bodyParser = require('body-parser')
 
+// 定义 app
+var app = express()
+
+// 开放静态资源
 app.use('/public', express.static('public'))
 app.use('/node_modules', express.static('../node_modules'))
 
+// 配置中间件 bodyParser, 使其能访问请求体
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// 配置模板引擎
 app.engine('html', require('express-art-template'))
 
+// 使用路由模块
 app.use(router)
 
+// 监听80端口
 app.listen(80, function () {
   console.log('server is running')
 })
--- nodejs/crud-express/router.js
+++ nodejs/crud-express/router.js
@@ -2,7 +2,7 @@
 * @Author: lifujiang
 * @Date:   2018-12-11 12:13:43
 * @Last Modified by:   lifujiang
-* @Last Modified time: 2018-12-11 21:58:44
+* @Last Modified time: 2018-12-12 21:12:42
 */
 // 该模块处理路由信息
 
@@ -11,16 +11,34 @@ var router = express.Router()
 var students = require('./students')
 
 // 封装错误信息提示
-function error (err, res) {
+function routerErr (err, res) {
   if (err) {
     res.status(500).send('error 500')
   }
 }
 
-// 网站路由
+// 封装跳转首页
+function redir (res) {
+  res.redirect('/students')
+}
+
+// 网站路由设计
+/*
+* | 请求方法  | 请求路径          | get参数 | post参数                       | 备注         |
+* | -------- | ---------------- | ------- | ------------------------------ | ------------|
+* | GET      | /students        |         |                                | 渲染主页页面 |
+* | GET      | /students/add    |         |                                | 渲染添加页面 |
+* | POST     | /students/add    |         | name, age, gender, hobbies     | 添加信息     |
+* | GET      | /students/update | id      |                                | 渲染更新页面 |
+* | POST     | /students/update |         | id, name, age, gender, hobbies | 更新信息     |
+* | GET      | /students/delete | id      |                                | 删除信息     |
+*/
+
 router.get('/students', function (req, res) {
+  // 渲染主页
+  // 拿到 students 模块显示数据的回调函数并渲染
   students.show(function (err, data) {
-    error(err, res)
+    routerErr(err, res)
     res.render('index.html', {
       students: data
     })
@@ -29,21 +47,28 @@ router.get('/students', function (req, res) {
 
 router.route('/students/add')
   .get(function (req, res) {
+    // 渲染添加页面
     res.render('add.html', {})
   })
 
   .post(function (req, res) {
+    // 处理添加信息
+    // 拿到 post 的请求体并传给 students 模块处理
+    // 并拿到回调函数判断操作是否成功, 成功 err 则为 null
     students.add(req.body, function (err) {
-      error(err, res)
+      routerErr(err, res)
       console.log('信息添加成功')
-      res.redirect('/students')
+      redir(res)
     })
   })
 
 router.route('/students/update')
   .get(function (req, res) {
+    // 渲染更新信息
+    // 拿到 url 的查询字符串并传给 students 模块处理
+    // 拿到回调函数返回的 data 并渲染修改更新页面
     students.showedit(req.query.id, function (err, data) {
-      if (err) error(err, res)
+      routerErr(err, res)
       res.render('edit.html', {
         students: data
       })
@@ -51,18 +76,20 @@ router.route('/students/update')
   })
 
   .post(function (req, res) {
+    // 与上面添加信息类似
     students.update(req.body, function (err) {
-      error(err)
+      routerErr(err)
       console.log('信息修改成功')
-      res.redirect('/students')
+      redir(res)
     })
   })
 
 router.get('/students/delete', function (req, res) {
   students.delete(req.query.id, function (err, data) {
-    error(err)
+    // 与上面添加信息类似
+    routerErr(err)
     console.log('信息删除成功')
-    res.redirect('/students')
+    redir(res)
   })
 })
 
